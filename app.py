1757526101eed479cbe0bc11e9697d27c3af6981
Vincent Yang <yf2005@gmail.com>
2023-02-17 23:02:56 +0800

    update file choice func

--- app.py
+++ app.py
@@ -130,10 +130,14 @@ def main():
                             )
                             # get proc_list and audit_list
                             subproc_list = proc_list[start_idx : end_idx + 1]
-                            # get basename of uploaded file
-                            basename = uploaded_file.name.split(".")[0]
-                            # save subproc_list to file using upload
-                            savedf(subproc_list, basename)
+                            # wp save button
+                            save_btn = st.sidebar.button("保存底稿")
+                            if save_btn:
+                                # get basename of uploaded file
+                                basename = uploaded_file.name.split(".")[0]
+                                # save subproc_list to file using upload
+                                savedf(subproc_list, basename)
+                                st.sidebar.success("保存成功")
 
                     else:
                         st.error("不支持文件类型")
@@ -151,177 +155,76 @@ def main():
             st.success("删除成功")
 
     elif choice == "文件选择":
+        # intialize session state
+        if "wp_choice" not in st.session_state:
+            st.session_state["wp_choice"] = ""
+        if "file_list" not in st.session_state:
+            st.session_state["file_list"] = []
         # choose radio for file1 or file2
-        file_choice = st.sidebar.radio("选择文件", ["选择文件1", "选择文件2"])
+        file_choice = st.sidebar.radio("选择文件", ["选择文件", "选择底稿"])
 
-        if file_choice == "选择文件1":
-            # get current file1 value from session
-            industry_choice = st.session_state["file1_industry"]
-            rule_choice = st.session_state["file1_rulechoice"]
-            filetype_choice = st.session_state["file1_filetype"]
-            section_choice = st.session_state["file1_section_list"]
-        elif file_choice == "选择文件2":
-            # get current file2 value from session
-            industry_choice = st.session_state["file2_industry"]
-            rule_choice = st.session_state["file2_rulechoice"]
-            filetype_choice = st.session_state["file2_filetype"]
-            section_choice = st.session_state["file2_section_list"]
-        # get preselected filetype index
-        file_typels = ["审计程序", "已上传文件"]
-        if filetype_choice == "":
-            filetype_index = 0
-        else:
-            filetype_index = file_typels.index(filetype_choice)
-        # choose file type
-        file_type = st.sidebar.selectbox(
-            "选择文件类型",
-            file_typels,
-            index=filetype_index,
-        )
-        
-        if file_type == "审计程序":
-
-            industry_list = get_folder_list(auditfolder)
-
-            # get preselected industry index
-            # if industry_choice in industry_list:
-            #     industry_index = industry_list.index(industry_choice)
-            # else:
-            industry_index = 0
-            rule_choice = []
-            section_choice = []
-
-            industry_choice = st.sidebar.selectbox(
-                "选择行业:", industry_list, index=industry_index
-            )
-
-            
-
-        elif file_type == "已上传文件":
-            if industry_choice != "":
-                rule_choice = []
+        if file_choice == "选择底稿":
+            # get current wp_choice value from session
+            wp_choice = st.session_state["wp_choice"]
             upload_list = get_uploadfiles()
-            upload_choice = st.sidebar.multiselect("选择已上传文件:", upload_list, rule_choice)
-            if upload_choice != []:
-                choosedf, choose_embeddings = get_upload_data(upload_choice)
-            else:
-                choosedf, choose_embeddings = None, None
-            industry_choice = ""
-            rule_choice = upload_choice
-            rule_column_ls = []
+            upload_choice = st.sidebar.selectbox("选择已上传文件:", upload_list)
+            
+            if upload_choice==[]:
+                st.error("请选择文件")
+                return
+            wp_choice = upload_choice
+
+        elif file_choice == "选择文件":
+            # get current file2 value from session
+            file_list = st.session_state["file_list"]
+
+            upload_list = get_uploadfiles()
+            upload_choice = st.sidebar.multiselect("选择已上传文件:", upload_list, file_list)
+            
+            if upload_choice==[]:
+                st.error("请选择文件")
+                return
+            
+            file_list = upload_choice
 
         # file choose button
         file_button = st.sidebar.button("选择文件")
         if file_button:
-            if file_choice == "选择文件1":
-                file1df, file1_embeddings = choosedf, choose_embeddings
-                file1_industry = industry_choice
-                file1_rulechoice = rule_choice
-                file1_filetype = file_type
-                file1_section_list = rule_column_ls
-                st.session_state["file1df"] = file1df
-                st.session_state["file1_embeddings"] = file1_embeddings
-                st.session_state["file1_industry"] = file1_industry
-                st.session_state["file1_rulechoice"] = file1_rulechoice
-                st.session_state["file1_filetype"] = file1_filetype
-                st.session_state["file1_section_list"] = file1_section_list
-                file2df = st.session_state["file2df"]
-                file2_embeddings = st.session_state["file2_embeddings"]
-                file2_industry = st.session_state["file2_industry"]
-                file2_rulechoice = st.session_state["file2_rulechoice"]
-                file2_filetype = st.session_state["file2_filetype"]
-                file2_section_list = st.session_state["file2_section_list"]
-            elif file_choice == "选择文件2":
-                file2df, file2_embeddings = choosedf, choose_embeddings
-                file2_industry = industry_choice
-                file2_rulechoice = rule_choice
-                file2_filetype = file_type
-                file2_section_list = rule_column_ls
-                st.session_state["file2df"] = file2df
-                st.session_state["file2_embeddings"] = file2_embeddings
-                st.session_state["file2_industry"] = file2_industry
-                st.session_state["file2_rulechoice"] = file2_rulechoice
-                st.session_state["file2_filetype"] = file2_filetype
-                st.session_state["file2_section_list"] = file2_section_list
-                file1df = st.session_state["file1df"]
-                file1_embeddings = st.session_state["file1_embeddings"]
-                file1_industry = st.session_state["file1_industry"]
-                file1_rulechoice = st.session_state["file1_rulechoice"]
-                file1_filetype = st.session_state["file1_filetype"]
-                file1_section_list = st.session_state["file1_section_list"]
+            if file_choice == "选择文件":
+                st.session_state["file_list"] = file_list
+                wp_choice = st.session_state["wp_choice"]
+            elif file_choice == "选择底稿":
+                st.session_state["wp_choice"] = wp_choice
+                file_list = st.session_state["file_list"]
         else:
-            file1df = st.session_state["file1df"]
-            file2df = st.session_state["file2df"]
-            file1_embeddings = st.session_state["file1_embeddings"]
-            file2_embeddings = st.session_state["file2_embeddings"]
-            file1_industry = st.session_state["file1_industry"]
-            file2_industry = st.session_state["file2_industry"]
-            file1_rulechoice = st.session_state["file1_rulechoice"]
-            file2_rulechoice = st.session_state["file2_rulechoice"]
-            file1_filetype = st.session_state["file1_filetype"]
-            file2_filetype = st.session_state["file2_filetype"]
-            file1_section_list = st.session_state["file1_section_list"]
-            file2_section_list = st.session_state["file2_section_list"]
+            file_list = st.session_state["file_list"]
+            wp_choice = st.session_state["wp_choice"]
 
         # file choose reset
         file_reset = st.sidebar.button("重置文件")
         if file_reset:
-            file1df, file2df = None, None
-            file1_embeddings, file2_embeddings = None, None
-            file1_industry, file2_industry = "", ""
-            file1_rulechoice, file2_rulechoice = [], []
-            file1_filetype, file2_filetype = "", ""
-            file1_section_list, file2_section_list = [], []
-            st.session_state["file1df"] = file1df
-            st.session_state["file2df"] = file2df
-            st.session_state["file1_embeddings"] = file1_embeddings
-            st.session_state["file2_embeddings"] = file2_embeddings
-            st.session_state["file1_industry"] = file1_industry
-            st.session_state["file2_industry"] = file2_industry
-            st.session_state["file1_rulechoice"] = file1_rulechoice
-            st.session_state["file2_rulechoice"] = file2_rulechoice
-            st.session_state["file1_filetype"] = file1_filetype
-            st.session_state["file2_filetype"] = file2_filetype
-            st.session_state["file1_section_list"] = file1_section_list
-            st.session_state["file2_section_list"] = file2_section_list
+            file_list = []
+            wp_choice = ""
+            st.session_state["file_list"] = file_list
+            st.session_state["wp_choice"] = wp_choice
 
-        st.subheader("已选择的文件1：")
+        st.subheader("已选择的文件：")
         # display file1 rulechoice
-        if file1_rulechoice != []:
+        if file_list != []:
             # convert to string
-            file1_rulechoice_str = "| ".join(file1_rulechoice)
+            file_choice_str = "| ".join(file_list)
             # display string
-            st.warning("文件1：" + file1_rulechoice_str)
+            st.warning("文件：" + file_choice_str)
         else:
-            st.error("文件1：无")
-        # display file1 section
-        if file1_section_list != []:
-            # convert to string
-            file1_section_str = "| ".join(file1_section_list)
-            # display string
-            st.info("章节1：" + file1_section_str)
-        else:
-            st.info("章节1：全部")
+            st.error("文件：无")
 
-        st.subheader("已选择的文件2：")
+        st.subheader("已选择的底稿：")
         # display file2 rulechoice
-        if file2_rulechoice != []:
-            # convert to string
-            file2_rulechoice_str = "| ".join(file2_rulechoice)
+        if wp_choice != "":
             # display string
-            st.warning("文件2：" + file2_rulechoice_str)
+            st.warning("底稿" + wp_choice)
         else:
-            st.error("文件2：无")
-
-        # display file2 section
-        if file2_section_list != []:
-            # convert to string
-            file2_section_str = "| ".join(file2_section_list)
-            # display string
-            st.info("章节2：" + file2_section_str)
-        else:
-            st.info("章节2：全部")
-
+            st.error("底稿：无")
 
     elif choice == "文件问答":
         st.subheader("文件问答")
