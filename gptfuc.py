3fab6e80b07d5101e44da7d69cad372f0042796f
Vincent Yang <yf2005@gmail.com>
2023-02-23 00:33:49 +0800

    get answer using frontend

--- gptfuc.py
+++ gptfuc.py
@@ -2,6 +2,8 @@ import os
 
 import requests
 from gpt_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from gpt_index import GPTSimpleVectorIndex, LLMPredictor
+from langchain import OpenAI
 
 # os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY")
 
@@ -11,7 +13,9 @@ backendurl = "http://localhost:8000"
 # openai_api_key=os.environ.get("OPENAI_API_KEY")
 # if openai_api_key is None:
 #     print("请设置OPENAI_API_KEY")
-
+llm_predictor = LLMPredictor(
+    llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=1024)
+)
 
 def build_index():
     documents = SimpleDirectoryReader(uploadfolder, recursive=True).load_data()
@@ -20,16 +24,27 @@ def build_index():
 
 
 def gpt_answer(question):
-    try:
-        url = backendurl + "/answer"
-        payload = {
-            "question": question,
-        }
-        headers = {}
-        res = requests.post(url, headers=headers, params=payload)
-        result = res.json()
-        print("成功")
-    except Exception as e:
-        print("错误: " + str(e))
-        result = "错误: " + str(e)
-    return result
+    filepath = os.path.join(uploadfolder, "filedata.json")
+    index = GPTSimpleVectorIndex.load_from_disk(filepath, llm_predictor=llm_predictor)
+
+    prompt = f'You are a helpful support agent. You are asked: "{question}". Try to use only the information provided. Format your answer nicely as a Markdown page.'
+    response = index.query(prompt).response.strip()
+    return response
+
+# def gpt_answer(question):
+#     try:
+#         url = backendurl + "/answer"
+#         payload = {
+#             "question": question,
+#         }
+#         headers = {}
+#         res = requests.post(url, headers=headers, params=payload)
+#         result = res.json()
+#         print("成功")
+#     except Exception as e:
+#         print("错误: " + str(e))
+#         result = "错误: " + str(e)
+#     return result
+
+
+
