2d1ff23535c201a8f45556e31a00085a2a96ab7b
vyang <yf2005@gmail.com>
2023-03-02 16:45:03 +0000

    test qa faiss

--- gptfuc.py
+++ gptfuc.py
@@ -2,11 +2,16 @@
 import json
 import os
 
-# from gpt_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
-from langchain import OpenAI
+from gpt_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
+# from langchain import OpenAI
 
 # import requests
-from llama_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
+# from llama_index import GPTSimpleVectorIndex, LLMPredictor, SimpleDirectoryReader
+
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 # read config from config.json
 with open("config.json", "r") as f:
@@ -28,14 +33,24 @@ if openai_api_key is None:
 else:
     print("已设置OPENAI_API_KEY" + openai_api_key)
 
-llm_predictor = LLMPredictor(
-    llm=OpenAI(temperature=0, model_name="text-davinci-003", max_tokens=1024)
-)
+# gpt_model="text-davinci-003"
+gpt_model='gpt-3.5-turbo'
+
+
+# llm_predictor = LLMPredictor(
+#     llm=OpenAI(temperature=0, model_name=gpt_model, max_tokens=1024)
+# )
+
+# use ChatGPT [beta]
+from gpt_index.langchain_helpers.chatgpt import ChatGPTLLMPredictor
+
+llm_predictor = ChatGPTLLMPredictor()
 
 
 def build_index():
     documents = SimpleDirectoryReader(filerawfolder, recursive=True).load_data()
-    index = GPTSimpleVectorIndex(documents)
+    # index = GPTSimpleVectorIndex(documents)
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor)
     index.save_to_disk(os.path.join(fileidxfolder, "filedata.json"))
 
 
@@ -45,7 +60,7 @@ def gpt_answer(question):
 
     # prompt = f'You are a helpful support agent. You are asked: "{question}". Try to use only the information provided. Format your answer nicely as a Markdown page.'
     prompt = f'您是一位专业顾问。您被问到："{question}"。请尽可能使用提供的信息。请将您的答案格式化为漂亮的Markdown页面。'
-    response = index.query(prompt).response.strip()
+    response = index.query(prompt,llm_predictor=llm_predictor)
     return response
 
 
---
+++ qa.py
@@ -0,0 +1,32 @@
+"""Ask a question to the notion database."""
+import faiss
+from langchain import OpenAI
+from langchain.chains import VectorDBQAWithSourcesChain
+import pickle
+import argparse
+import os
+import json
+# read config from config.json
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# get openai api key from config.json
+api_key = config["openai_api_key"]
+
+os.environ["OPENAI_API_KEY"] = api_key
+
+parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
+parser.add_argument('question', type=str, help='The question to ask the notion DB')
+args = parser.parse_args()
+
+# Load the LangChain.
+index = faiss.read_index("docs.index")
+
+with open("faiss_store.pkl", "rb") as f:
+    store = pickle.load(f)
+
+store.index = index
+chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+result = chain({"question": args.question})
+print(f"Answer: {result['answer']}")
+print(f"Sources: {result['sources']}")
